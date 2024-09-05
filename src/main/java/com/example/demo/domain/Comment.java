7eaa541f4d507df6e6d258f20e34bb2cec58f793
Jo Eungyeong <joeungyeong23@gmail.com>
2024-09-05 15:48:34 +0900

    update comment posting API

--- src/main/java/com/example/demo/domain/Comment.java
+++ src/main/java/com/example/demo/domain/Comment.java
@@ -35,7 +35,7 @@ public class Comment {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String writeDatetime = simpleDateFormat.format(now);
 
-        this.content = dto.getComment();
+        this.content = dto.getContent();
         this.writeDatetime = writeDatetime;
         this.userEmail = email;
         this.boardId = boardId;
